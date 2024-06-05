a700f673de97569baf9b3ab9d5b0e31ab9b1e19c
Jack Dolby <92309491+JackDolbs@users.noreply.github.com>
2024-06-05 15:38:24 +0100

    fix to about images

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,30 +13,30 @@ export default function About() {
   return (
     <div>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-10 my-20">
-        {images.map((image, index) => (
-          <motion.div
-            key={image}
-            initial={{
-              opacity: 0,
-              y: -50,
-              rotate: 0,
-            }}
-            animate={{
-              opacity: 1,
-              y: 0,
-              rotate: index % 2 === 0 ? 3 : -3,
-            }}
-            transition={{ duration: 0.2, delay: index * 0.1 }}
-          >
-            <Image
-              src={image}
-              width={200}
-              height={400}
-              alt="about"
-              className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60 hover:rotate-0 transition duration-200"
-            />
-          </motion.div>
-        ))}
+      {images.map((image, index) => (
+        <motion.div
+          key={index}
+          initial={{
+            opacity: 0,
+            y: -50,
+            rotate: 0,
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+            rotate: index % 2 === 0 ? 3 : -3,
+          }}
+          transition={{ duration: 0.2, delay: index * 0.1 }}
+        >
+          <Image
+            src={image}
+            width={200}
+            height={400}
+            alt="about"
+            className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60 hover:rotate-0 transition duration-200"
+          />
+        </motion.div>
+      ))}
         {/* 
         // <Image
         //   src="https://images.unsplash.com/photo-1692544350322-ac70cfd63614?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
